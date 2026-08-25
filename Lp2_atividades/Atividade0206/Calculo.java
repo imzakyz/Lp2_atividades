7235cb07d585bfe91121419465f4a39b1bce7fa7
Isaac Moreira <isaac.moreira.bsi@outlook.com>
2026-08-25 10:41:50 -0300

    Adicionando alguns arquivos

---
+++ Lp2_atividades/Atividade0206/Calculo.java
@@ -0,0 +1,17 @@
+public class Calculo{
+    // variavel para guardar o total do salário
+    private double totalSalario;
+
+    // método para calcular o salario
+    // as variaveis dentro do metodo são locais e não podem ser acessadas fora do método
+    protected void calculaSalario(double salarioBase, double horasTrabalhadas){
+        // o this é usado para referenciar a variavel da classe
+        this.totalSalario = salarioBase * horasTrabalhadas;
+    }
+
+    // método para retornar o total do salário
+    public double getTotalSalario(){
+        return this.totalSalario;
+    }
+
+}
---
+++ Lp2_atividades/Atividade0206/Funcionario.java
@@ -0,0 +1,15 @@
+package Atividade0206;
+
+public class Funcionario{
+
+    private String nome;
+    private String cpf;
+    private String rg;
+    private String dataContrato;
+    private double salario;
+    private double salarioBase;
+    private int horasTrabalhadas;
+    private int matricula;
+    
+
+}
